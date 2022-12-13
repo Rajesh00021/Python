44721f235a53420daa9e9039a6ccc56a53d7fcab
Rajesh Deshbandhu <rajeshdeshbandhu0@gmail.com>
2022-12-14 00:54:24 +0530

    Add files via upload

---
+++ Relational_Operators.py
@@ -0,0 +1,15 @@
+a=10
+b=20
+print(a<b) # True  < operator
+print(a>b) # False > operator
+print(a==b) # False  == operator
+print(a<=b) # True  <= operator
+print(a>=b) # False  >= operator
+print(a!=b) # True  != operator
+
+x = "Amitabh "
+y= "Bachchan"
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
