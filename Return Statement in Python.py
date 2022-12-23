45a4557df046cfad04ccacab1e35bb07eea9abd4
Rajesh Deshbandhu <rajeshdeshbandhu0@gmail.com>
2022-12-23 17:23:42 +0530

    Update Return Statement in Python.py

--- Return Statement in Python.py
+++ Return Statement in Python.py
@@ -1,1 +1,9 @@
-# Python
+# Python return statement 
+# A return statement is overall used to invoke a function so that the passed statements can be executed
+
+def fun(x,y):
+  return (x+y)
+
+res = fun(10,20)
+print("Result: ".format(res))
+
