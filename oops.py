f600f828c75242b3a2d0b91ffdffa565431766e5
Rajesh Deshbandhu <rajeshdeshbandhu0@gmail.com>
2022-12-29 17:14:39 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,12 @@
+# class Student:
+    
+#     def __init__(self,sid,sname,sclass):
+#         self.sid=sid
+#         self.sname=sname
+#         self.sclass=sclass
+        
+#     def m1(self):    
+#        print("Student ID:",self.sid,"\n","Student Name:",self.sname,"\n","Student Class:",self.sclass)
+# s1 = Student(100,"Rajesh",12)
+# s1.m1()
+
